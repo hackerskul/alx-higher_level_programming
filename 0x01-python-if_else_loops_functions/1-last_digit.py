5c3007fc24af6e37aba2b8a8d3a856a8259e6282
hackerskul <hackerskull254@gmail.com>
2023-03-14 05:56:49 -0400

    number detection, positive 0 or negative

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,4 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 last = abs(number) % 10
-print(f"Last digit of {number} is {last} {'and is gerater than 5' if last > 5 else ''} {'and is 0' if last == 0 else ''} {'and is less than 6 and not 0' if last < 6 and last != 0 else ''}")
+mess = f"Last digit of {number} is {last}"
+if number < 0:
+    mess = f"Last digit of {number} is -{last}"
+if last > 5:
+    print(mess + " and is greater than 5")
+elif last == 0:
+    print(mess + " and is 0")
+elif last < 6 and last != 0:
+    print(mess + " and is less than 6 and not 0")
