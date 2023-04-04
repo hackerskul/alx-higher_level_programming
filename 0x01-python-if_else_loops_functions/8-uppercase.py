a94552c5d6efaa5b275e5e66cf27191a51671cc3
hackerskul <hackerskull254@gmail.com>
2023-04-04 09:06:33 -0400

    upper or lower using ord and chr

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in str:
-        if ord(str) >= 97 and ord(str) <= 127:
-            i = chr(ord(str)) - 32
+        if ord(str) >= 97 and ord(str) <= 122:
+            i = chr(ord(str) - 32)
             print("{}".format(c), end="")
     print("")
