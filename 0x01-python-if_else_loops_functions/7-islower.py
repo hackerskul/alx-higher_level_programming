6de197c02f86c60f8b2b2979c77293e363e11d5c
hackerskul <hackerskull254@gmail.com>
2023-04-04 08:32:38 -0400

    upper or lower using ord

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,5 +2,5 @@
 def islower(c):
     if ord(c) >= 97 and ord(c) <= 127:
         print(c, "is lower")
-    else if ord(c) >= 65 and ord(c) <= 90:
+    elif ord(c) >= 65 and ord(c) <= 90:
         print(c, "is upper")
