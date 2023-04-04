6b8a9441e0d176b2f3f4c3cb3bb31de4fffc1aca
hackerskul <hackerskull254@gmail.com>
2023-04-04 09:48:08 -0400

    last nmbr

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,8 +2,10 @@
 def fizzbuzz():
     for i in range(100 + 1):
         if i % 3 == 0:
-            print("Fizz")
+            print("Fizz"," ")
         elif i % 5 == 0:
-            print("Buzz")
+            print("Buzz"," ")
         elif i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz")
+            print("FizzBuzz", " ")
+        else:
+            print(i," ")
