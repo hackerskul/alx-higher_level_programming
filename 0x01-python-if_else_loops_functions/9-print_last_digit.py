8889d922e3a588eb6a84e3c5fb5aa05b391054bf
hackerskul <hackerskull254@gmail.com>
2023-04-04 09:33:41 -0400

    last nmbr

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-	lst = str(number)[-1]
-	print(int(lst))
+    """Print the last digit of a number and return it."""
+    print(abs(number) % 10, end="")
+    return (abs(number) % 10)
