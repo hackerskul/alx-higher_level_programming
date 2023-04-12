4b37744e18dcfa5c62845c09d64634e60f7dfa57
hackerskul <hackerskull254@gmail.com>
2023-04-12 07:06:51 -0400

    returning a new list

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def no_c(my_string):
+
+    new_list = []
+
+    for i in my_string:
+        if not in ['c', 'C']:
+            new_list.append(i)
+    new_string = ''.join(new_list)
+
+    return new_string
