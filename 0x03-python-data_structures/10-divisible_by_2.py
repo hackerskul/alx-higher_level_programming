6ec483e85094170898479d03e7a654a554b93a59
hackerskul <hackerskull254@gmail.com>
2023-04-13 03:02:10 -0400

    playing around with funcs

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    for i in my_list:
+    for i in range(len(my_list)):
         new = []
         if my_list[i] % 2 == 0:
             new.append(True)
