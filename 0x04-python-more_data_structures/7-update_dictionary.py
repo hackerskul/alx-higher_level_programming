f0d8d117414840f66144d944d1eefc8e3c0cee0c
hackerskul <hackerskull254@gmail.com>
2023-04-17 04:48:04 -0400

    unique vals

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key in a_dictionary:
-        a_dictionary[key] = value
-    else:
-        a_dictionary.get(key, value)
+    a_dictionary[key] = value
