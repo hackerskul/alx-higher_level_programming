f85e733e4a08f84081977793ebba077da0aaf06f
hackerskul <hackerskull254@gmail.com>
2023-04-17 05:27:15 -0400

    multiply the values in a dict

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    for key in a_dictionary:
-        a_dictionary[key] = * 2
-    return a_dictionary
+    new = {}
+    for key,value in a_dictionary.items():
+        new[key] = value * 2
+    return new
