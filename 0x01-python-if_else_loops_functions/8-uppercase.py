f84079e9dc1acba39710434eaef0ae7b2afe9a8c
hackerskul <hackerskull254@gmail.com>
2023-12-14 03:09:01 -0500

    pycode

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for char in str:
+        if ord('a') <= ord(char) <= ord('z'):
+            print("{:c}".format(ord(char) - ord('a') + ord('A')), end="")
+        else:
+            print("{}".format(char), end="")
+    print()
