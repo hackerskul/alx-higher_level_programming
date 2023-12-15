c656f3fdf470e5b5605ffb8c61769b2ed9c2f1f2
hackerskul <hackerskull254@gmail.com>
2023-12-15 05:26:05 -0500

    pycode

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-from add_0 import add
-
+from 0-add import add
+# Define a and b on separate lines
 a = 1
 b = 2
 
-print("{} + {} = {}".format(a, b, add(a, b)))
+# Check if the script is being run as the main program
+if __name__ == "__main__":
+    # Print the result using string formatting
+    print("{} + {} = {}".format(a, b, add(a, b)))
