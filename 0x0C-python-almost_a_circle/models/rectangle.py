fc4568e3d3000a53b8fb2adc2d3df0a5c09f1daf
hackerskul <hackerskull254@gmail.com>
2024-01-19 14:40:44 -0500

    python documentation for function update self

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,12 +95,8 @@ class Rectangle(Base):
             [print("#", end="") for w in range(self.width)]
             print("")
 
-    def __str__(self):
-        """returns a formated string of tha values id width height etc"""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
-
     def update(self, *args, **kwargs):
-                """Update the Rectangle.
+        """Update the Rectangle.
 
         Args:
             *args (ints): New attribute values.
@@ -114,9 +110,9 @@ class Rectangle(Base):
         if args and len(args) != 0:
             a = 0
             for arg in args:
-                if a = 0:
+                if a == 0:
                     if arg is None:
-                        self.__init__(self.width,self.height,self.x,self.y)
+                        self.__init__(self.width, self.height, self.x, self.y)
                     else:
                         self.id = arg
                 elif a == 1:
@@ -145,3 +141,18 @@ class Rectangle(Base):
                 elif k == "y":
                     self.y = v
 
+    def to_dictionary(self):
+        """Return the dictionary representation of a Rectangle."""
+        return {
+            "id": self.id,
+            "width": self.width,
+            "height": self.height,
+            "x": self.x,
+            "y": self.y
+        }
+
+    def __str__(self):
+        """Return the print() and str() representation of the Rectangle."""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                       self.x, self.y,
+                                                       self.width, self.height)
